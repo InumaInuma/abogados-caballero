defef9394d7a23681e193dfca99f53135879365c
YOGA <Anthony_Inuma@hotmail.com>
2021-05-18 20:39:28 -0500

    seccion1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PorfolioComponent } from './pages/porfolio/porfolio.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PorfolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contactos', component: ContactosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { PorfolioComponent } from './pages/porfolio/porfolio.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactosComponent,
+    ItemComponent,
+    AboutComponent,
+    BlogComponent,
+    PorfolioComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
